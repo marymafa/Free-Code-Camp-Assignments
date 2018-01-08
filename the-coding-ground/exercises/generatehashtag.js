911ae22ad77681e19ed5dbaef4206bcce5c3dc1c
MaryMafa <marymafa80@gmail.com>
2018-01-08 14:14:54 +0000

    added new file

---
+++ the-coding-ground/exercises/generatehashtag.js
@@ -0,0 +1,21 @@
+
+
+function dblLinear(n) {
+  var ai = 0, bi = 0, eq = 0;
+  var sequence = [1];
+  while (ai + bi < n + eq) {
+    var y = 2 * sequence[ai] + 1;
+    var z = 3 * sequence[bi] + 1;
+    if (y < z) { sequence.push(y); ai++; }
+    else if (y > z) { sequence.push(z); bi++; }
+    else { sequence.push(y); ai++; bi++; eq++; }
+  }
+  sequence.forEach(function(item){
+    console.log(item);
+  })
+  return sequence.pop();
+}
+
+
+
+console.log(dblLinear(50));
--- the-coding-ground/exercises/symmetric-difference.js
+++ the-coding-ground/exercises/symmetric-difference.js
@@ -2,17 +2,29 @@
 returns an array of the symmetric difference (△ or ⊕) of the provided array.*/
 
 
-function sym(arr1,arr2) {
-   var newArr = arr1.concat(arr2);
+function sym() {
+    var args = [];
+    for (var i = 0; i < arguments.length; i++) {
+        args.push(arguments[i]);
+      }
 
-  function check(item) {
-    if (arr1.indexOf(item) === -1 || arr2.indexOf(item) === -1) {
-      return item;
+      function symDiff(arr1,arr2){
+      var results=[];
+      arr1.forEach(function(item){
+        if(arr2.indexOf(item)<0){
+          results.push(item);
+        }
+      })
+      arr2.forEach(function(item){
+        if(arr1.indexOf(item)<0){
+          results.push(item);
+        }
+      })
+      return results;
     }
-  }
-
-  return newArr.filter(check);
-
-
+    return args.reduce(symDiff);
 }
-console.log(sym([1, 2, 3], [5, 2, 1, 4],[1,3,5]));
+
+console.log(sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]));
+
+
