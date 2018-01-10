85c451bba65e1ca6452066c8d287e71f9124331e
MaryMafa <marymafa80@gmail.com>
2018-01-10 11:29:15 +0000

    added remove function and sort function

--- the-coding-ground/exercises/inventor-update.js
+++ the-coding-ground/exercises/inventor-update.js
@@ -3,18 +3,32 @@ function mergingTwoArray(arr1, arr2) {
     var newArr = arr1.concat(arr2);
     return newArr;
 }
+function remove(array, element) {
+    const index = array.indexOf(element);
+    return array.splice(index, 1);
+}
 function updateInventory(arr1, arr2) {
-  arr2.forEach(function (element) {
+    arr2.forEach(function (element) {
         for (var index = 0; index < arr1.length; index++) {
             if (element[1] === arr1[index][1]) {
                 element[0] += arr1[index][0];
+                remove(arr1, arr1[index])
             }
         }
-           if (arr1.indexOf(element) === -1) {
+
+        if (arr1.indexOf(element) === -1) {
             arr1.push(element);
         }
-})
-return arr1
+    })
+    arr1.sort(function (a, b) {
+        if (a[1] < b[1]) {
+            return -1;
+        }else if(a[1]>b[1]){
+             return 1;
+        }
+
+    })
+    return arr1
 }
 var curInv = [
     [21, "Bowling Ball"],
