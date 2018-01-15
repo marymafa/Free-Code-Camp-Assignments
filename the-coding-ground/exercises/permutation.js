4b77c4e738abf2456d887df92788c6b498c927fc
MaryMafa <marymafa80@gmail.com>
2018-01-15 13:23:07 +0000

    made changes

--- the-coding-ground/exercises/permutation.js
+++ the-coding-ground/exercises/permutation.js
@@ -1,28 +1,26 @@
-var temp="";
-function generate(n,a) {
-    temp = a;
-    if (n === 1) {
-        console.log(a);
-    } else {
-        // for i := 0; i < n - 1; i += 1 do
-        //     generate(n - 1, A)
-        //     if n is even then
-        // swap(A[i], A[n - 1])
-        for (var i = 0; i <= n-1; i++) {
-            generate(n-1, temp);
-            if (n % 2 === 0) {
-                let split = temp.split("");
-                split.splice(i,1, temp[n-1]);
-                split.splice(n-1,1, temp[i]);
-                temp = split.join("");
-            } else {
-                let split = temp.split("");
-                split.splice(0,1, temp[n-1]);
-                split.splice(n-1,1, temp[0]);
-                temp = split.join("");
-            }
-        }
-        generate(n-1, temp);
+var temp;
+function generate(n, str){
+  temp = str;
+  if (n === 1){
+    console.log(str);
+  } else {
+    for (var i = 0; i < n - 1; i++){
+      generate(n-1, temp);
+
+      if (n % 2 === 0){
+        let split = temp.split("");
+        split.splice(i, 1, temp[n-1]);
+        split.splice(n-1, 1, temp[i]);
+        temp = split.join("");
+      } else {
+        let split = temp.split("");
+        split.splice(0, 1, temp[n-1]);
+        split.splice(n-1, 1, temp[0]);
+        temp = split.join("");
+      }
     }
+    generate(n - 1, temp);
+  }
 }
-console.log(generate("aab"));
+
+generate(3, "aab");
