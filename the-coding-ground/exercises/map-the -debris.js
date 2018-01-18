c7ca354f8ce8d4a36cf5fb8d8c37651f4cda1fc1
MaryMafa <marymafa80@gmail.com>
2018-01-18 16:13:47 +0000

    added new code

---
+++ the-coding-ground/exercises/map-the -debris.js
@@ -0,0 +1,33 @@
+function orbitalPeriod(averageAltitudes) {
+    var GM = 398600.4418;
+    var earthRadius = 6367.4447;
+    var pow = 2;
+    var i = 0;
+    var arr=[];
+
+    for (var i in averageAltitudes) {
+        //console.log(averageAltitudes[i].name);
+        
+        var avgAlt = averageAltitudes[i].avgAlt;
+        var a = earthRadius + avgAlt;
+        var cube = a * a * a;
+        var orbitalPeriod = (Math.PI * 2) * Math.sqrt(cube / GM);
+        arr.push({name: averageAltitudes[i].name, orbitalPeriod:Math.round(orbitalPeriod)})
+    }
+
+    return arr;
+
+}
+
+//console.log(orbitalPeriod([{ name: "sputnik", avgAlt: 35873.5553 }]));
+console.log(orbitalPeriod([{ name: "iss", avgAlt: 413.6 }, { name: "hubble", avgAlt: 556.7 }, { name: "moon", avgAlt: 378632.553 }]))
+
+// Return a new array that transforms the element's average altitude into their orbital periods.
+
+// The array will contain objects in the format {name: 'name', avgAlt: avgAlt}.
+
+// You can read about orbital periods on wikipedia.
+
+// The values should be rounded to the nearest whole number. The body being orbited is Earth.
+
+// The radius of the earth is 6367.4447 kilometers, and the GM value of earth is 398600.4418 km3s-2.
