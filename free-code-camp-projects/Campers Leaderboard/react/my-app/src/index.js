c5439e59994860a8b4fa8ff05ad01ae043454bd6
marymafa <marymafa80@gmail.com>
2018-04-03 16:38:21 +0200

    modified a file

--- free-code-camp-projects/Campers Leaderboard/react/my-app/src/index.js
+++ free-code-camp-projects/Campers Leaderboard/react/my-app/src/index.js
@@ -1,17 +1,26 @@
-  import React from "react";
-  import ReactDOM from "react-dom";
-  import './index.css';
-  import axios from 'axios';
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.css';
+import axios from 'axios';
 
-export default class Persontdst extends React.Component {
+class Camper extends React.Component {
   state = {
     campers: []
   }
 
+  recent(){
+    axios.get(
+      `https://fcctop100.herokuapp.com/api/fccusers/top/recent`
+    )
+     .then(res => {
+        const campers = res.data;
+        this.setState({ campers });
+      })
+    
+  }
   componentDidMount() {
-    axios.get(`https://fcctop100.herokuapp.com/api/fccusers/top/recent`,
-    `https://fcctop100.herokuapp.com/api/fccusers/top/alltime`
-
+    axios.get(
+      `https://fcctop100.herokuapp.com/api/fccusers/top/alltime`
     )
 
       .then(res => {
@@ -21,47 +30,47 @@ export default class Persontdst extends React.Component {
   }
 
   render() {
-      
+
     return (
-        <div>
+      <div>
         <a class="footer-font" href="https://codepen.io/marymafa">Coded by Mary Mafa</a>
-        <div>Campers Leaderboard</div>
-<table>
-<thead>
-<tr>
-<th>
-img
-</th>
-<th>
-username
-</th>
-<th>
-recent
-</th>
-<th>
-lastUpdate
-</th>
-<th>
-alltime
-</th>
-</tr>
-</thead>
-<tbody>
- { 
-            this.state.campers.map(campers =>
+        <div>Camper's Leaderboard</div>
+        <table>
+          <thead>
             <tr>
-            <td><img alt=""  src={campers.img}/></td>
-            <td>{campers.username}</td>
-            <td>{campers.recent}</td>
-            <td>{campers.alltime}</td>
-            <td>{campers.lastUpdate}</td>
+              <th>
+                img
+</th>
+              <th>
+                username
+</th>
+              <th>
+                recent
+</th>
+              <th>
+                lastUpdate
+</th>
+              <th>
+                alltime
+</th>
             </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.campers.map(campers =>
+                <tr>
+                  <td><img alt="" src={campers.img} /></td>
+                  <td>{campers.username}</td>
+                  <td>{campers.recent}</td>
+                  <td>{campers.alltime}</td>
+                  <td>{campers.lastUpdate}</td>
+                </tr>
+              )
+            }
+          </tbody>
+        </table>
+      </div>
     )
   }
-  </tbody>
-  </table>
-  </div>
-  )
 }
-}
-  ReactDOM.render(<Persontdst/>,document.getElementById('root'))
+ReactDOM.render(<Camper />, document.getElementById('root'));
