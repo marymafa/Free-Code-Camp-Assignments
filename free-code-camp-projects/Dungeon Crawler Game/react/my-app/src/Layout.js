a8cfb2a9d06302557544d3bde5d7cd143a64c5b2
marymafa <marymafa80@gmail.com>
2018-06-13 12:26:38 +0200

    added a function on each button

--- free-code-camp-projects/Dungeon Crawler Game/react/my-app/src/Layout.js
+++ free-code-camp-projects/Dungeon Crawler Game/react/my-app/src/Layout.js
@@ -8,35 +8,49 @@ export default class Layout extends React.Component {
             grid: dungeon.creatingPath(),
             enemies: [],
         }
+        console.log("enemiese", this.state.enemies)
     }
     componentDidMount() {
+        this.myGamePiece()
         this.randomValues()
         this.setState({ grid: dungeon.creatingPath() });
-        // var user = this.state.grid.find(e => {
-        //     return e.x === 5 && e.y === 0
-        // });
     }
     GetRandomEnemies() {
         var getEnenmies = this.state.enemies;
-        for (var i = 0; i < 10; i++) {
-            var randomEnemies = { x: Math.floor(Math.random() * 10), y: Math.floor(Math.random() * 10), enemies: <p className="icon">&#x2639;</p> };
+        for (var i = 0; i < 8; i++) {
+            var randomEnemies = { x: Math.floor(Math.random() * 10), y: Math.floor(Math.random() * 10), enemies: <p> &#x26DF;</p> };
             if (getEnenmies.indexOf(randomEnemies) === -1) {
                 getEnenmies.push(randomEnemies)
             }
         }
-
         return getEnenmies;
     }
-
     randomValues() {
         var random = this.GetRandomEnemies();
         this.setState({
             grid: dungeon.creatingPath(random),
         })
-        console.log("hey", random)
 
     }
-
+    myGamePiece() {
+        var player = <p className="icon">&#x26F9;</p>;
+    }
+    moveUp() {
+        this.myGamePiece.speedY = +1;
+        console.log("up", this.myGamePiece.speedY)
+    }
+    moveDown() {
+        this.myGamePiece.speedY = -1;
+        console.log("down", this.myGamePiece.speedY)
+    }
+    moveLeft() {
+        this.myGamePiece.speedX = -1;
+        console.log("left", this.myGamePiece.speedY)
+    }
+    moveRight() {
+        this.myGamePiece.speedX = +1;
+        console.log("right", this.myGamePiece.speedY)
+    }
     render() {
         return (
             <div>
@@ -53,12 +67,16 @@ export default class Layout extends React.Component {
                             element.display = <p className="icon"> &#x2692;</p>
                         } else if (element.x === 1 && element.y === 2) {
                             element.display = <p className="icon"> &#x26DF;</p>
-                        }else {
+                        } else {
                             element.display = null;
                         }
                         return <button className="grid" id={element.pathway} >{element.display}</button>
                     })
                 }</div>
+                <button onClick={this.moveUp.bind(this)}>UP</button>
+                <button onClick={this.moveDown.bind(this)}>DOWN</button>
+                <button onClick={this.moveLeft.bind(this)}>LEFT</button>
+                <button onClick={this.moveRight.bind(this)}>RIGHT</button>
             </div>
         )
     }
