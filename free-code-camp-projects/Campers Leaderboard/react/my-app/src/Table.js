7b5a10b9b171d3692d98afeb282f29e9b8240fbb
TCGPC1 <you@example.com>
2019-01-31 12:34:39 +0200

    made changes

--- free-code-camp-projects/Campers Leaderboard/react/my-app/src/Table.js
+++ free-code-camp-projects/Campers Leaderboard/react/my-app/src/Table.js
@@ -5,29 +5,37 @@ export default class Camper extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-         Table: []
+            table: [],
         }
 
 
     }
 
-    getCamper(Api) {
-        axios.get("https://fcctop100.herokuapp.com/api/fccusers/top/" + Api)
-        .then(res => {
-            this.setState({ Table: res.Table });
-        })
-}
+    componentDidMount() {
+        var campers = axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/alltime').then(function (result) {
+            this.setState({
+                table: result.data
+            });
+        });
+        console.log("alltime", campers)
+    }
+    getRecent() {
+        var campers = axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/recent').then(function (result) {
+            this.setState({
+                table: result.data
+            });
+        });
+        console.log("recent", campers)
+    }
 
-  
     render() {
-
+        console.log("state", this.state)
         return (
             <div>
                 <a class="footer-font" href="https://codepen.io/marymafa">Coded by Mary Mafa</a>
                 <div>Camper's Leaderboard</div>
                 <div>
-                    <button bsStyle="success" onClick={this.getCamper.bind(this)}>Click here for recent</button>
-                    <button bsStyle="success" onClick={this.getCamper.bind(this)}>Click here for alltime</button>
+                    <button bsStyle="success" onClick={this.getRecent.bind(this)}>Click here for recent</button>
                 </div>
                 <table>
                     <thead>
@@ -51,13 +59,13 @@ export default class Camper extends React.Component {
                     </thead>
                     <tbody>
                         {
-                            this.state.Table.map(Table =>
+                            this.state.table.map(camper =>
                                 <tr>
-                                    <td><img alt="" src={Table.img} /></td>
-                                    <td>{Table.username}</td>
-                                    <td>{Table.recent}</td>
-                                    <td>{Table.alltime}</td>
-                                    <td>{Table.lastUpdate}</td>
+                                    <td><img alt="" src={camper.img} /></td>
+                                    <td>{camper.username}</td>
+                                    <td>{camper.recent}</td>
+                                    <td>{camper.alltime}</td>
+                                    <td>{camper.lastUpdate}</td>
                                 </tr>
                             )
                         }
